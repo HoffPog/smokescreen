d0daf48f7efa0bc9d411a4404124f4fbff6e1467
yoMama <smasheder73@icloud.com>
2024-10-28 20:32:33 +0000

    added a lil bit of metadata stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import React from 'react';
 import MyForm from "./form";
+import type { Metadata } from "next";
 
-
-
-
+export const metadata: Metadata = {
+  title: 'EVE Smokescreen'
+}
 
 export default function Home() {
   return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+
+
+
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
           <h1
             className="text-sky-400"
           >Would be a real shame if you wanted to know someones characterID</h1>
